0bb4cce8522dc26cb96ffc170b16130aa8cb8ed3
dabin-im <112664372+dabin-im@users.noreply.github.com>
2024-02-04 15:14:01 -0800

    [Dabin Im] Initialized project setting named "PicTALK" and built efficient structure

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+
+import SigninForm from "./_auth/forms/SigninForm.tsx";
+import SignupForm from "./_auth/forms/SignupForm.tsx";
+import { Home } from "./_root/pages";
+import "./globals.css";
+import AuthLayout from "./_auth/AuthLayout.tsx";
+import RootLayout from "./_root/RootLayout.tsx";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
