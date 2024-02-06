68635c5a93eb2d2364c48af825b56cd506b4ed4e
dabin-im <112664372+dabin-im@users.noreply.github.com>
2024-02-05 23:00:20 -0800

    Implemented Sign-in and Sign-up webpages (front-end)

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,5 +1,13 @@
 import { z } from "zod";
 
 export const SignupValidation = z.object({
-  username: z.string().min(2).max(50),
+  name: z.string().min(2, "Too short"),
+  username: z.string().min(2, "Too short"),
+  email: z.string().email(),
+  password: z.string().min(8, "password must be at least 8 characters."),
+});
+
+export const SigninValidation = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, "password must be at least 8 characters."),
 });
